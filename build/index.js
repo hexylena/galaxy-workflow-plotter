549567f860cc75ee7ab750a3ba74f7d1e1a6cc87
Eric Rasche <rasche.eric@gmail.com>
2016-07-12 17:09:33 +0000

    Updates

--- build/index.js
+++ build/index.js
@@ -34,7 +34,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "build/";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
@@ -17191,8 +17191,8 @@
 	    entry: ["./index.js"],
 	    output: {
 	        path: __dirname + "/build",
-	        filename: "main.js",
-	        publicPath: "",
+	        filename: "index.js",
+	        publicPath: "build/",
 	    },
 	    module: {
 	        loaders: [
