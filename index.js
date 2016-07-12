f00086d923d1eddcc780c678a6d80b8f89a0b132
Eric Rasche <rasche.eric@gmail.com>
2016-07-12 16:52:04 +0000

    Fix build for GH deployment

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-require("./bower_components/google-material-icons/dist/material-icons-font.min.css");
-require("./bower_components/Materialize/dist/css/materialize.min.css");
+//require("./bower_components/google-material-icons/dist/material-icons-font.min.css");
 require("./index.css");
-require("./bower_components/jquery/dist/jquery.js");
-require("./bower_components/Materialize/dist/js/materialize.min.js");
-require("./bower_components/d3/d3.min.js");
 require("./bower_components/file-saver/FileSaver.js");
 require("./index.js");
 require.context("./", false, /^\.\/.*\.html/);
-
 
 var d3 = require("./bower_components/d3/d3.js");
 var margin = {top: -5, right: -5, bottom: -5, left: -5},
